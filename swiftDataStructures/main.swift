33206edbd732acc7e5fce12f25cfd1b8e94fdf47
Ethan Lillie <lillieethan@gmail.com>
2014-12-22 10:52:22 -0500

    Initial Commit

---
+++ swiftDataStructures/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  swiftDataStructures
+//
+//  Created by Ethan Lillie on 12/22/14.
+//  Copyright (c) 2014 Ethan Lillie. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
